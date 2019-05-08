8fcaca3522afa4f3a5fd615dcad542b8c0b01e18
jkurthoconnor <jkurthoconnor@gmail.com>
2019-05-07 20:35:43 -0600

    Render individual routes to table

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import React, { Component } from 'react';
 import './App.css';
+import data from './data.js'; // how to import directly as in const?
+
+const { routes, airlines, airports } = data;
 
 class App extends Component {
+
   render() {
+    const routeRows = routes.map( (route, idx) => (
+      <tr key={idx}>
+        <td>{route.airline}</td>
+        <td>{route.src}</td>
+        <td>{route.dest}</td>
+      </tr>
+    ));
+
     return (
       <div className="app">
         <header className="header">
@@ -10,8 +22,13 @@ class App extends Component {
         </header>
         <section>
           <p>
-            Welcome to the app!
+            Welcome to the Routes!
           </p>
+          <table>
+            <tbody>
+              {routeRows}
+            </tbody>
+          </table>
         </section>
       </div>
     );
